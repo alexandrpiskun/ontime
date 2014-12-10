86bb464be5a0426439523bc96a9183bb8cb26291
Ilya Figotin <ifigotin@gmail.com>
2014-12-09 20:12:32 -0800

    Preconditions

--- server/src/main/java/com/ontime/model/Chart.java
+++ server/src/main/java/com/ontime/model/Chart.java
@@ -1,13 +1,15 @@
 package com.ontime.model;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class Chart {
   private final String id;
   private final String data;
   private final int severity;
 
   public Chart(String id, String data, int severity) {
-    this.id = id;
-    this.data = data;
+    this.id = checkNotNull(id, "id is missing");
+    this.data = checkNotNull(data, "data is missing");
     this.severity = severity;
   }
   
