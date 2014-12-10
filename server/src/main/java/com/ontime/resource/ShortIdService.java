73e063a32358bde9f157ad4054b0ea05cc912870
Ilya Figotin <ifigotin@gmail.com>
2014-12-09 16:30:01 -0800

    Added sample datastore usage

---
+++ server/src/main/java/com/ontime/resource/ShortIdService.java
@@ -0,0 +1,25 @@
+package com.ontime.resource;
+
+public class ShortIdService {
+
+  public static final String ALPHABET = "23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_";
+  public static final int BASE = ALPHABET.length();
+
+  public static String encode(long id) {
+      StringBuilder str = new StringBuilder();
+      while (id > 0) {
+          str.insert(0, ALPHABET.charAt((int)(id % BASE)));
+          id = id / BASE;
+      }
+      return str.toString();
+  }
+
+  public static long decode(String strId) {
+      long id = 0;
+      for (int i = 0; i < strId.length(); i++) {
+          id = id * BASE + ALPHABET.indexOf(strId.charAt(i));
+      }
+      return id;
+  }
+  
+}
